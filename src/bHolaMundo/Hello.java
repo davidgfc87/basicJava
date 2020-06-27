15e38fa545ab0683ca779a001dc3af162d691b18
unknown <dgfc87@gmail.com>
2020-06-27 10:23:16 -0500

    Commit a pseudocodigo

--- src/bHolaMundo/Hello.java
+++ src/bHolaMundo/Hello.java
@@ -3,6 +3,7 @@ package bHolaMundo.fHelloWorld;
 public class Hello {
 
 	public static void main(String args[]) {
+
 		System.out.println("hello, world\n");  //funcion para imprimir en consola
 	}
 }
